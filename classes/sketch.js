e1bbce2a83077459a2c472c00c23133640cbd9b1
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-11 15:48:35 -0400

    Update sketch.js

--- classes/sketch.js
+++ classes/sketch.js
@@ -11,9 +11,60 @@ class Asteroid{
     this.x = 0
     this.y = random(30, height - 30);
     this.v = random(2,4);
-    this.r = random(15,40)
+    this.r = random(15,40);
+  }
+  update() {
+
+    this.x += this.v;
+  
+    // if reset is needed: pick random diameter and reset x
+    if (this.x > width + this.r){
+      this.r = random(15,40);
+      this.x = 0 - this.r;
+  
+    }
+      
   }
 }
+
+// _color is a variable
+class Spaceship {
+  constructor(_color){
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.vx = random(1,2);
+    this.vy = random(1, 2);
+    this.color = _color;
+  }
+
+  update(){
+    this.x += this.vx;
+    this.y += this.vy;
+
+    if (this.x > width){
+      this.x -= width;
+    }
+    if (this.y > height){
+      this.y -= height;
+    }
+  }
+
+
+}
+
+
+
+function draw(){
+  background("lightblue")
+  noStroke();
+  fill(this.color)
+  rect(this.x, this.y, 50, 15)
+}
+
+
+let spaceship0;
+let spaceship1;
+
 
 
 function setup() {
@@ -30,6 +81,7 @@ function setup() {
     // };
     asteroidArray.push(myAsteroid);
   }
+  spaceship0 = new Spaceship("steel")
 }
 
 function draw() {
@@ -38,6 +90,7 @@ function draw() {
 
   for (let i = 0; i < asteroidArray.length; i += 1) {
     let myAsteroid = asteroidArray[i];
+    myAsteroid.update();
 
     // draw circles
     ellipse(myAsteroid.x, myAsteroid.y, 2 * myAsteroid.r, 2 * myAsteroid.r);
